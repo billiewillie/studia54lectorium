d2672f61c35b005f83517bce9763d639e29801e7
Vadim Belinovich <vadimbelinovich@gmail.com>
2022-09-14 19:10:40 +0300

    added

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,7 @@ import {Fancybox} from "@fancyapps/ui";
 const backdrop = document.querySelector('.backdrop');
 
 new Swiper('.gallery__list', {
-  modules: [ Navigation, Pagination ],
+  modules: [Navigation, Pagination],
   slidesPerView: 1,
   navigation: {
     nextEl: ".swiper-button-next",
@@ -44,7 +44,15 @@ const breakpointChecker = function () {
 
 const enableSwiper = function () {
   mySwiper = new Swiper('.expectations__list', {
+    modules: [Pagination],
     slidesPerView: 1,
+    pagination: {
+      el: ".expectations__pagination",
+      clickable: true,
+      renderBullet: function (index, className) {
+        return `<span class="${className}"></span>`;
+      },
+    },
   });
 };
 
@@ -115,3 +123,57 @@ document.querySelector('.button').addEventListener('click', function () {
     backdrop.classList.remove("hide", "show");
   }, 300);
 });
+
+// const header = document.querySelector("header");
+// const headerObserver = new IntersectionObserver(
+//   ([entry]) => {
+//     if (entry.isIntersecting) {
+//       entry.target.classList.add('shown');
+//       console.log(entry);
+//     }
+//   }, {
+//     rootMargin: '-300px 0px 0px 0px',
+//     threshold: 0,
+//   }
+// );
+//
+// headerObserver.observe(header)
+// let lastScroll = 0;
+//
+// const throttle = (func, time = 100) => {
+//   let lastTime = 0;
+//   return () => {
+//     const now = new Date();
+//     if (now - lastTime >= time) {
+//       func();
+//       time = now;
+//     }
+//   };
+// };
+//
+// const validateHeader = () => {
+//   const windowY = window.scrollY;
+//   const windowH = window.innerHeight;
+//
+//   if (windowY > windowH) {
+//     // We passed the first section, set a toggable class
+//     header.classList.add("is-fixed");
+//     // Determine is we ready to animate
+//     if (windowY > windowH + 40) {
+//       header.classList.add("can-animate");
+//       if (windowY < lastScroll) {
+//         // Determine if we scrolling up
+//         header.classList.add("scroll-up");
+//       } else {
+//         header.classList.remove("scroll-up");
+//       }
+//     } else {
+//       header.classList.remove("scroll-up");
+//     }
+//   } else {
+//     header.classList.remove("is-fixed", "can-animate");
+//   }
+//   lastScroll = windowY;
+// };
+//
+// window.addEventListener("scroll", throttle(validateHeader, 100));

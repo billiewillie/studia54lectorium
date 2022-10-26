07df75b1c09494ead672b6d3886d74026d5c545c
Vadim Belinovich <vadimbelinovich@gmail.com>
2022-10-26 19:30:50 +0300

    test

--- src/js/app.js
+++ src/js/app.js
@@ -321,7 +321,7 @@ gsap.to(".ring2", {
 gsap.to(".speaker-ring", {
 	scrollTrigger: {
 		trigger: ".speaker__photo",
-		start: "100% 80%",
+		start: "100% 50%",
 		onEnter: () => {
 			speakerRing.style.top = "500px";
 			ringInner.style.height = `233px`;
@@ -339,7 +339,7 @@ gsap.to(".speaker-ring", {
 gsap.to(".speaker-ring", {
 	scrollTrigger: {
 		trigger: ".speaker__photo",
-		start: "100% +80%",
+		start: "100% 150%",
 		onEnter: () => {
 			speakerRing.style.top = "900px";
 			speakerRing.classList.add("speaker3");

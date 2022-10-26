6b802a2b6229dbf0886b8088d21b46cd25932c9d
Vadim Belinovich <vadimbelinovich@gmail.com>
2022-10-26 19:03:49 +0300

    test

--- src/js/app.js
+++ src/js/app.js
@@ -226,7 +226,7 @@ gsap.to(".ring", {
 gsap.to(".ring2", {
 	scrollTrigger: {
 		trigger: ".quote",
-		start: "30$ 100%",
+		start: "40$ 100%",
 		onEnter: () => {
 			ring2.style.top = quoteTop;
 			ringInner2.style.height = `216px`;

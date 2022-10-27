bab34a792db108756a0c6ef002ffbbb2609aecd6
Vadim Belinovich <vadimbelinovich@gmail.com>
2022-10-27 09:48:25 +0300

    test

--- src/js/app.js
+++ src/js/app.js
@@ -214,7 +214,7 @@ ringInner.style.width = `${firstCircle.getBoundingClientRect().width}px`;
 gsap.to(".ring", {
 	scrollTrigger: {
 		trigger: ".first-screen",
-		start: "100% -120%",
+		start: "100% 100%",
 		onEnter: () => {
 			ring.style.top = firstCircleCoordTop;
 			ringInner.style.height = `${firstCircle.getBoundingClientRect().height}px`;

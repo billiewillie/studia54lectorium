c191e5cd98d6337a60834fa8f35481c5075cf4c9
Vadim Belinovich <vadimbelinovich@gmail.com>
2022-10-27 15:01:31 +0300

    test

--- src/js/app.js
+++ src/js/app.js
@@ -371,7 +371,7 @@ document.addEventListener("DOMContentLoaded", () => {
 	gsap.to(".qa-ring", {
 		scrollTrigger: {
 			trigger: ".qa",
-			start: "30% 100%",
+			start: "-10% 100%",
 			onEnter: () => {
 				qaRing.style.top = "0";
 				qaRing.classList.add("show");
@@ -385,7 +385,7 @@ document.addEventListener("DOMContentLoaded", () => {
 	gsap.to(".qa-ring", {
 		scrollTrigger: {
 			trigger: ".qa",
-			start: "90% 90%",
+			start: "140% 100%",
 			onEnter: () => {
 				qaRing.style.top = "700px";
 				qaRing.classList.add("qa2");

4e5df7053a257170b2857bf3ef3ea0a50bc743db
Vadim Belinovich <vadimbelinovich@gmail.com>
2022-10-27 12:09:04 +0300

    test

--- src/js/app.js
+++ src/js/app.js
@@ -198,205 +198,193 @@ popupClose.addEventListener("click", () => {
 	}, 300);
 });
 
-const firstCircle = document.querySelector(".first__circle");
-const firstCircleCoordTop = `${document.querySelector(".first__circle").getBoundingClientRect().top}px`;
-const quoteTop = `${document.querySelector(".quote").getBoundingClientRect().top - 50}px`;
-const detailsTop = `${document.querySelector(".details").getBoundingClientRect().top - 150}px`;
-const detailsBottom = `${document.querySelector(".details").getBoundingClientRect().bottom - 400}px`;
-const expectationsTop = `${document.querySelector(".expectations").getBoundingClientRect().top + 160}px`;
-const groupTop = `${document.querySelector(".group").getBoundingClientRect().top}px`;
+document.addEventListener("DOMContentLoaded", () => {
+	const firstCircle = document.querySelector(".first__circle");
+	let firstCircleCoordTop;
+	setTimeout(() => {
+		firstCircleCoordTop = `${document.querySelector(".first__circle").getBoundingClientRect().top}px`;
+	});
+	console.log(document.querySelector(".first__circle").getBoundingClientRect().top);
+	const quoteTop = `${document.querySelector(".quote").getBoundingClientRect().top - 100}px`;
+	const detailsTop = `${document.querySelector(".details").getBoundingClientRect().top - 150}px`;
+	const detailsBottom = `${document.querySelector(".details").getBoundingClientRect().bottom - 400}px`;
+	const expectationsTop = `${document.querySelector(".expectations").getBoundingClientRect().top + 250}px`;
+	const groupTop = `${document.querySelector(".group").getBoundingClientRect().top}px`;
 
-ring.style.top = firstCircleCoordTop;
-ringInner.style.height = `${firstCircle.getBoundingClientRect().height}px`;
-ringInner.style.width = `${firstCircle.getBoundingClientRect().width}px`;
+	// ring.style.top = firstCircleCoordTop;
+	ringInner.style.height = `${firstCircle.getBoundingClientRect().height}px`;
+	ringInner.style.width = `${firstCircle.getBoundingClientRect().width}px`;
 
-gsap.to(".ring", {
-	scrollTrigger: {
-		trigger: ".first-screen",
-		start: "80% 100%",
-		onEnter: () => {
-			ring.style.top = firstCircleCoordTop;
-			ringInner.style.height = `${firstCircle.getBoundingClientRect().height}px`;
-			ringInner.style.width = `${firstCircle.getBoundingClientRect().width}px`;
-			document.querySelector(".ring").classList.add("show");
+	gsap.to(".ring", {
+		scrollTrigger: {
+			trigger: ".first-screen",
+			start: "100% 100%",
+			onEnter: () => {
+				ring.style.top = firstCircleCoordTop;
+				ringInner.style.height = `${firstCircle.getBoundingClientRect().height}px`;
+				ringInner.style.width = `${firstCircle.getBoundingClientRect().width}px`;
+				document.querySelector(".ring").classList.add("show");
+			},
 		},
-	},
-});
-gsap.to(".ring2", {
-	scrollTrigger: {
-		trigger: ".quote",
-		start: "40$ 100%",
-		onEnter: () => {
-			ring2.style.top = quoteTop;
-			ringInner2.style.height = `216px`;
-			ringInner2.style.width = `216px`;
-			document.querySelector(".ring2").classList.add("quote");
+	});
+	gsap.to(".ring2", {
+		scrollTrigger: {
+			trigger: ".details",
+			start: "30% 100%",
+			onEnter: () => {
+				ring2.style.top = detailsTop;
+				ringInner2.style.height = `163px`;
+				ringInner2.style.width = `163px`;
+				document.querySelector(".ring2").classList.add("details");
+			},
+			onLeaveBack: () => {
+				ring2.style.top = quoteTop;
+				ringInner2.style.height = `216px`;
+				ringInner2.style.width = `216px`;
+				document.querySelector(".ring2").classList.remove("details");
+			},
 		},
-		onLeaveBack: () => {
-			ring2.style.top = firstCircleCoordTop;
-			ringInner2.style.height = `0px`;
-			ringInner2.style.width = `0px`;
-			document.querySelector(".ring2").classList.remove("quote");
+	});
+	gsap.to(".ring2", {
+		scrollTrigger: {
+			trigger: ".details",
+			start: "80% 100%",
+			onEnter: () => {
+				ring2.style.top = detailsBottom;
+				ringInner2.style.height = `213px`;
+				ringInner2.style.width = `213px`;
+				document.querySelector(".ring2").classList.add("detailsBottom");
+			},
+			onLeaveBack: () => {
+				ring2.style.top = detailsTop;
+				ringInner2.style.height = `163px`;
+				ringInner2.style.width = `163px`;
+				document.querySelector(".ring2").classList.remove("detailsBottom");
+			},
 		},
-	},
-});
-gsap.to(".ring2", {
-	scrollTrigger: {
-		trigger: ".details",
-		start: "30% 100%",
-		onEnter: () => {
-			ring2.style.top = detailsTop;
-			ringInner2.style.height = `163px`;
-			ringInner2.style.width = `163px`;
-			document.querySelector(".ring2").classList.add("details");
+	});
+	gsap.to(".ring2", {
+		scrollTrigger: {
+			trigger: ".expectations",
+			start: "100% 100%",
+			onEnter: () => {
+				ring2.style.top = expectationsTop;
+				ringInner2.style.height = `187px`;
+				ringInner2.style.width = `187px`;
+				document.querySelector(".ring2").classList.add("expectations");
+			},
+			onLeaveBack: () => {
+				ring2.style.top = detailsBottom;
+				ringInner2.style.height = `213px`;
+				ringInner2.style.width = `213px`;
+				document.querySelector(".ring2").classList.remove("expectations");
+			},
 		},
-		onLeaveBack: () => {
-			ring2.style.top = quoteTop;
-			ringInner2.style.height = `216px`;
-			ringInner2.style.width = `216px`;
-			document.querySelector(".ring2").classList.remove("details");
+	});
+	gsap.to(".ring2", {
+		scrollTrigger: {
+			trigger: ".group",
+			start: "100% 100%",
+			onEnter: () => {
+				ring2.style.top = groupTop;
+				document.querySelector(".ring2").classList.add("group");
+				ringInner2.style.height = `233px`;
+				ringInner2.style.width = `233px`;
+			},
+			onLeaveBack: () => {
+				ring2.style.top = expectationsTop;
+				ringInner2.style.height = `233px`;
+				ringInner2.style.width = `233px`;
+				document.querySelector(".ring2").classList.remove("group");
+			},
 		},
-	},
-});
-gsap.to(".ring2", {
-	scrollTrigger: {
-		trigger: ".details",
-		start: "80% 100%",
-		onEnter: () => {
-			ring2.style.top = detailsBottom;
-			ringInner2.style.height = `213px`;
-			ringInner2.style.width = `213px`;
-			document.querySelector(".ring2").classList.add("detailsBottom");
+	});
+	gsap.to(".speaker-ring", {
+		scrollTrigger: {
+			trigger: ".speaker__photo",
+			start: "-30% 50%",
+			onEnter: () => {
+				speakerRing.style.top = "500px";
+				ringInner.style.height = `233px`;
+				ringInner.style.width = `233px`;
+				speakerRing.classList.add("speaker2");
+			},
+			onLeaveBack: () => {
+				speakerRing.style.top = "0";
+				ringInner.style.height = `100%`;
+				ringInner.style.width = `100%`;
+				speakerRing.classList.remove("speaker2");
+			},
 		},
-		onLeaveBack: () => {
-			ring2.style.top = detailsTop;
-			ringInner2.style.height = `163px`;
-			ringInner2.style.width = `163px`;
-			document.querySelector(".ring2").classList.remove("detailsBottom");
+	});
+	gsap.to(".speaker-ring", {
+		scrollTrigger: {
+			trigger: ".speaker__photo",
+			start: "100% -50%",
+			onEnter: () => {
+				speakerRing.style.top = "900px";
+				speakerRing.classList.add("speaker3");
+			},
+			onLeaveBack: () => {
+				speakerRing.style.top = "500px";
+				speakerRing.classList.remove("speaker3");
+			},
 		},
-	},
-});
-gsap.to(".ring2", {
-	scrollTrigger: {
-		trigger: ".expectations",
-		start: "100% 100%",
-		onEnter: () => {
-			ring2.style.top = expectationsTop;
-			ringInner2.style.height = `187px`;
-			ringInner2.style.width = `187px`;
-			document.querySelector(".ring2").classList.add("expectations");
+	});
+	gsap.to(".speaker-ring", {
+		scrollTrigger: {
+			trigger: ".interview",
+			start: "70% +100%",
+			onEnter: () => {
+				speakerRing.style.top = "1450px";
+				speakerRing.classList.add("interview");
+			},
+			onLeaveBack: () => {
+				speakerRing.style.top = "900px";
+				speakerRing.classList.remove("interview");
+			},
 		},
-		onLeaveBack: () => {
-			ring2.style.top = detailsBottom;
-			ringInner2.style.height = `213px`;
-			ringInner2.style.width = `213px`;
-			document.querySelector(".ring2").classList.remove("expectations");
+	});
+	gsap.to(".speaker-ring", {
+		scrollTrigger: {
+			trigger: ".interview",
+			start: "100% +140%",
+			onEnter: () => {
+				speakerRing.style.top = "1900px";
+				speakerRing.classList.add("interview2");
+			},
+			onLeaveBack: () => {
+				speakerRing.style.top = "1450px";
+				speakerRing.classList.remove("interview2");
+			},
 		},
-	},
-});
-gsap.to(".ring2", {
-	scrollTrigger: {
-		trigger: ".group",
-		start: "100% 100%",
-		onEnter: () => {
-			ring2.style.top = groupTop;
-			document.querySelector(".ring2").classList.add("group");
-			ringInner2.style.height = `233px`;
-			ringInner2.style.width = `233px`;
+	});
+	gsap.to(".qa-ring", {
+		scrollTrigger: {
+			trigger: ".qa",
+			start: "0% 50%",
+			onEnter: () => {
+				qaRing.style.top = "0";
+				qaRing.classList.add("show");
+			},
+			onLeaveBack: () => {
+				qaRing.style.top = "0";
+				qaRing.classList.remove("show");
+			},
 		},
-		onLeaveBack: () => {
-			ring2.style.top = expectationsTop;
-			ringInner2.style.height = `233px`;
-			ringInner2.style.width = `233px`;
-			document.querySelector(".ring2").classList.remove("group");
+	});
+	gsap.to(".qa-ring", {
+		scrollTrigger: {
+			trigger: ".qa",
+			start: "100% +80%",
+			onEnter: () => {
+				qaRing.style.top = "400px";
+				qaRing.classList.add("qa2");
+			},
+			onLeaveBack: () => {
+				qaRing.style.top = "0";
+				qaRing.classList.remove("qa2");
+			},
 		},
-	},
-});
-gsap.to(".speaker-ring", {
-	scrollTrigger: {
-		trigger: ".speaker__photo",
-		start: "-30% 50%",
-		onEnter: () => {
-			speakerRing.style.top = "500px";
-			ringInner.style.height = `233px`;
-			ringInner.style.width = `233px`;
-			speakerRing.classList.add("speaker2");
-		},
-		onLeaveBack: () => {
-			speakerRing.style.top = "0";
-			ringInner.style.height = `100%`;
-			ringInner.style.width = `100%`;
-			speakerRing.classList.remove("speaker2");
-		},
-	},
-});
-gsap.to(".speaker-ring", {
-	scrollTrigger: {
-		trigger: ".speaker__photo",
-		start: "100% -50%",
-		onEnter: () => {
-			speakerRing.style.top = "900px";
-			speakerRing.classList.add("speaker3");
-		},
-		onLeaveBack: () => {
-			speakerRing.style.top = "500px";
-			speakerRing.classList.remove("speaker3");
-		},
-	},
-});
-gsap.to(".speaker-ring", {
-	scrollTrigger: {
-		trigger: ".interview",
-		start: "70% +100%",
-		onEnter: () => {
-			speakerRing.style.top = "1450px";
-			speakerRing.classList.add("interview");
-		},
-		onLeaveBack: () => {
-			speakerRing.style.top = "900px";
-			speakerRing.classList.remove("interview");
-		},
-	},
-});
-gsap.to(".speaker-ring", {
-	scrollTrigger: {
-		trigger: ".interview",
-		start: "100% +140%",
-		onEnter: () => {
-			speakerRing.style.top = "1900px";
-			speakerRing.classList.add("interview2");
-		},
-		onLeaveBack: () => {
-			speakerRing.style.top = "1450px";
-			speakerRing.classList.remove("interview2");
-		},
-	},
-});
-gsap.to(".qa-ring", {
-	scrollTrigger: {
-		trigger: ".qa",
-		start: "0% 50%",
-		onEnter: () => {
-			qaRing.style.top = "0";
-			qaRing.classList.add("show");
-		},
-		onLeaveBack: () => {
-			qaRing.style.top = "0";
-			qaRing.classList.remove("show");
-		},
-	},
-});
-gsap.to(".qa-ring", {
-	scrollTrigger: {
-		trigger: ".qa",
-		start: "100% +80%",
-		onEnter: () => {
-			qaRing.style.top = "400px";
-			qaRing.classList.add("qa2");
-		},
-		onLeaveBack: () => {
-			qaRing.style.top = "0";
-			qaRing.classList.remove("qa2");
-		},
-	},
+	});
 });
